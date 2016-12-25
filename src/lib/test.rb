99ab0d61433f3b3df811a8a95a4186bca048dbd6
tenpy0 <kounara.kouda@gmail.com>
2016-12-25 20:03:02 +0900

    Topic add comment (#8)
    * initializeメソッドのコメントを追加
    
    * コメント追加: greeting

--- src/lib/test.rb
+++ src/lib/test.rb
@@ -1,7 +1,13 @@
+# coding: utf-8
 class Test
+  # 初期化
+  # @param name [String] 名前
   def initialize(name)
     @name = name
   end
+
+  # 挨拶
+  # @return [String]
   def greeting
     "hello, #{@name}"
   end
